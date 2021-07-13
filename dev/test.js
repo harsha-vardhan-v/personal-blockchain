31fba7133ed6542d1d2110de3a79a18c6f1555d8
harsha-vardhan-v <v.harshavardhan93@gmail.com>
2021-07-13 16:04:35 +0530

    Added proof of work and genesis

--- dev/test.js
+++ dev/test.js
@@ -1,6 +1,7 @@
 const Blockchain = require('./blockchain')
 
 const cryptoCurrency = new Blockchain();
+console.log('Genesis', cryptoCurrency);
 
 const previousBlockHash = '87765DA6CCF0668238C1D27C35692E11';
 const currentBlockData = [
@@ -20,7 +21,9 @@ const currentBlockData = [
         recipient: '3A3F6E42D48E9',
     },
 ];
-const nonce = 100;
-const hash = cryptoCurrency.hashBlock(previousBlockHash, currentBlockData, nonce);
 
+const nonce = cryptoCurrency.proofOfWork(previousBlockHash, currentBlockData);
+console.log(nonce);
+
+const hash = cryptoCurrency.hashBlock(previousBlockHash, currentBlockData, nonce);
 console.log(hash);
